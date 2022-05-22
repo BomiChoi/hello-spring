ea6fcad4b36600148ea06f8308decdaf95960013
Bomi Choi <hibomi97@gmail.com>
2022-05-22 22:30:01 +0900

    스프링 DB 접근 기술

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,22 +1,37 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.*;
+import hello.hellospring.service.MemberService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import hello.hellospring.service.MemberService;
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-	
+//	private final DataSource dataSource;
+//	private final EntityManager em;
+	private final MemberRepository memberRepository;
+
+	@Autowired
+	public SpringConfig(MemberRepository memberRepository) {
+//		this.dataSource = dataSource;
+//		this.em = em;
+		this.memberRepository = memberRepository;
+	}
+
 	@Bean
 	public MemberService memberService() {
-		return new MemberService(memberRepository());
+		return new MemberService(memberRepository);
 	}
-	
-	@Bean
-	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
-	}
+
+//	@Bean
+//	public MemberRepository memberRepository() {
+////		return new MemoryMemberRepository();
+////		return new JdbcMemberRepository(dataSource);
+////		return new JdbcTemplateMemberRepository(dataSource);
+//		return new JpaMemberRepository(em);
+//	}
 }
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,12 +3,12 @@ package hello.hellospring.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 
+@Transactional
 public class MemberService {
 
 	private final MemberRepository memberRepository;
@@ -23,7 +23,7 @@ public class MemberService {
 	public Long join(Member member) {
 		validateDuplicateMember(member); // 중복 회원 검증
 		memberRepository.save(member);
-		return member.getId();
+		return member.getId();		
 	}
 
 	private void validateDuplicateMember(Member member) {
